df09728da92f31248a2f17be766ad57bd424e018
王瑶 <cq.xiaoer@gmail.com>
2019-12-14 21:09:24 +0800

    请求完成

--- pages/pc/index.jsx
+++ pages/pc/index.jsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
+
 const Index = props => {
-    return (
-        <div className="header">
-            <style jsx>{`
+  return (
+    <div className="header">
+      <style jsx>{`
                 .header{
                     font-size:18px;
                 }
             `}</style>
 
-            这里是PC端项目首页
-            <div className='footer'>1</div>
-        </div>
-    )
+      这里是PC端项目首页
+      <div className='footer'>1</div>
+      用移动端课查看功能演示
+    </div>
+  )
 };
 
 export default Index
